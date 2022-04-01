062c798ad8f05dd27570ab8cdbbd526146fd26dd
Alex Muniz <alemuniz@motorola.com>
2022-04-01 08:53:07 -0400

    css styling & date/currency format

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt'); // configura LOCALE para pt-BR
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' }, // LOCALE_ID é para transformar a data em formato BR
+    {
+      provide: DEFAULT_CURRENCY_CODE, // DEFAULT_CURRENCY_CODE é para transformar a moeda em formato R$
+      useValue: 'BRL',
+    },
+  ],
   bootstrap: [AppComponent],
   exports: [],
 })
