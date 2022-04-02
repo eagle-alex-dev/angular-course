57acf77d7326fc907954eed1a80c598e62fa9658
Alex Muniz <alemuniz@motorola.com>
2022-04-02 18:10:17 -0400

    integrar html com service e consumir api de login

--- gatito-book/src/app/home/login/login.component.ts
+++ gatito-book/src/app/home/login/login.component.ts
@@ -1,15 +1,29 @@
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './../../authentication/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.authenticate(this.usuario, this.senha).subscribe({
+      next: () => {
+        console.log(`Usuário ${this.usuario} autenticado com sucesso.`);
+      },
+      error: (error) => {
+        alert('Usuário ou senha inválida');
+        console.log(error);
+      },
+    });
   }
-
 }
