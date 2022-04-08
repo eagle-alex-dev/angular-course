ed0270a4333543b71ff8c071e294459d05c2b5fe
Alex Muniz <alemuniz@motorola.com>
2022-04-08 15:07:15 -0400

    create form novo-usuario

--- gatito-book/src/app/home/home.module.ts
+++ gatito-book/src/app/home/home.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomeRoutingModule } from './home-routing.module';
+import { MessageModule } from '../components/message/message.module';
 
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { MessageModule } from '../components/message/message.module';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
   imports: [CommonModule, HomeRoutingModule, FormsModule, MessageModule],
   exports: [HomeComponent],
 })
