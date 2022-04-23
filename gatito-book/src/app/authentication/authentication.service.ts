ddd67ac4250e68a1f81a6094222d0939d9ec941a
Alex Muniz <alemuniz@motorola.com>
2022-04-23 12:09:21 -0400

    salva token do usuario no localStorage

--- gatito-book/src/app/authentication/authentication.service.ts
+++ gatito-book/src/app/authentication/authentication.service.ts
@@ -1,6 +1,7 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +9,30 @@ import { Observable } from 'rxjs';
 export class AuthenticationService {
   private url = 'http://localhost:3000/user/login';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  authenticate(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post(this.url, {
-      userName: usuario,
-      password: senha,
-    });
+  // o tipo da resposta Observable<HttpResponse<any>> é porque agora eu quero pegar a requisição inteira,
+  // e não somente o body, que é o padrão do Angular
+  authenticate(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        this.url,
+        {
+          userName: usuario,
+          password: senha,
+        },
+        { observe: 'response' } // pega toda a informação da requisição - body e header
+      )
+      .pipe(
+        // indica q toda vez q houver o post, eu quero fazer uma operação - "salvar o token"
+        tap((res) => {
+          // tap é a função que é usada quando não me interessa a resposta da operação que eu vou executar
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken); // salva o token no nosso localStorage (usuarioSubject)
+        })
+      );
   }
 }
